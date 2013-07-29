eb8fea619f4f12a12809a3f65e5ebf7b3e2b58c3
Mateusz-Krupa <matkrupa@gmail.com>
2013-07-30 00:32:54 +0200

    Infinite scroll

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,16 @@ angular.module('flashAPIApp', [])
             ctrl.$render();
         }
     };
+    }).directive('whenScrolled', function() {
+        return function(scope, elm, attr) {
+            var raw = elm[0];
+
+            elm.bind('scroll', function() {
+                if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                    scope.$apply(attr.whenScrolled);
+                }
+            });
+        };
     })
     .factory('pageModel',function(){
 
@@ -43,10 +53,14 @@ angular.module('flashAPIApp', [])
         $scope.items = [];
         $scope.tech = 'html';
         $scope.administrator = true;
+        $scope.currentPage = 1;
+        $scope.totalPages = 6;
 
         $scope.setTech = function(text){
             $scope.tech = text;
         };
+
+
 
         $scope.save = function(items){
             pageModel.save(items);
@@ -56,5 +70,27 @@ angular.module('flashAPIApp', [])
         function init(){
             $scope.items = pageModel.getItems();
         };
+
+        var counter = 0;
+        $scope.loadMore = function() {
+            for (var i = 0; i < 2; i++) {
+                $scope.currentPage = counter;
+                var value =
+                {
+                    "title" : "Html5 player Api",
+                    "desc" : "ble ble ble ble"
+                };
+                if($scope.items.length < $scope.totalPages ){
+                    $scope.items.push(value);
+                    counter += 1;
+                }
+                    console.log($scope.items);
+
+            }
+        };
+
+        $scope.loadMore();
+
+
 
     });
